3740c1f8e98bbc5dbe3fe5514d68f39e005afeca
Dan Sylvain <dansylvain84@gmail.com>
2020-06-30 15:51:39 +0200

    ultime modif avant favicon

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Un blog perso fait avec <span className="icon -love"><RiHeart2Line/></span> par <Link to="/">Dan</Link></p>
+      <p>Un blog de voyage fait avec <span className="icon -love"><RiHeart2Line/></span> par <Link to="/">Dan</Link></p>
     </div>
   </footer>
 )
